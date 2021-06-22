1cba261fcfcc0f5b0d7f2fad4dd8a11599364725
FMirzaei93 <firoozeh.mirzaei@gmail.com>
2021-06-22 10:05:36 +0200

    updated for using it's function in another js file(drag_and_drop.js)

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
+var warning_alert = "Do you really want to Delete this item?";
+var cross_signs = null;
+var images_urls = [];
+var current_images_urls = [];
+var images_containers = null;
+
+
 $(document).ready(function() {
 
-    var warning_alert = "Do you really want to Delete this item?";
-    var cross_signs = $('div.imggalery div.btngalery img');
-    var images_urls = [];
-    var current_images_urls = [];
-    var images_containers = $('div.imggalery');
-
+    cross_signs = $('div.imggalery div.btngalery img');
+    images_containers = $('div.imggalery');
 
 
     // ---------------- Retrieving Images From Images Directory -------------
@@ -23,64 +26,12 @@ $(document).ready(function() {
     });
 
 
-
     // ---------------- Clicking on upload button --------------------------------
 
     $('div#header > div').click(function() {
 
         uploadingNewImages();
     });
-
-
-    //----------------- Monitoring the Images directory and adding the new ones -------
-
-    function uploadingNewImages() {
-
-        $.ajax({
-            url: "./images.factory.php",
-            type: "GET",
-            data: { function_name: 'imagesFindAllFunc' },
-            dataType: "JSON",
-            success: function(response, status) {
-
-                current_images_urls = response;
-
-                var difference_array = [];
-                difference_array = $(current_images_urls).not(images_urls).get();
-
-                if (current_images_urls.length > images_urls.length) {
-
-                    $.each(difference_array, function(index, value) {
-
-                        updateThePageAfterUploading(value);
-                    });
-
-                }
-            }
-        });
-
-    }
-
-    // ---------------- Function for adding new image (from Directory) into user's browser ------------------
-
-    function updateThePageAfterUploading(src) {
-
-
-        var new_image_container = document.createElement('div');
-        $(new_image_container).addClass("imggalery");
-        new_image_container.innerHTML = `<div class="contentgalery"><img style="margin:5px 5px;" src="${src}" width="160" height="100"></div><div class="btngalery"><img style="cursor:pointer;" src="images/cross.gif"></div>`;
-        var images_zone = $('div#images_zone');
-        images_zone.append(new_image_container);
-
-        images_urls.push(src);
-        images_containers = $('div.imggalery');
-
-
-        $(new_image_container).find('div.btngalery img').on("click", function(event) {
-            crossButtonClick($(event.target));
-        });
-
-    }
 
 
     // ---------------- Clicking on each Cross --------------------------------
@@ -140,59 +91,57 @@ $(document).ready(function() {
 
     }
 
-
-    //-------------------------------------------
-
-    // // preventing page from redirecting
-    // $("html").on("dragover", function(e) {
-    //     e.preventDefault();
-    //     e.stopPropagation();
-    // });
-    // $("html").on("drop", function(e) {
-    //     e.preventDefault();
-    //     e.stopPropagation();
-    // });
-
-    // // Drag enter
-    // $('div#page').on('dragenter', function(e) {
-    //     e.stopPropagation();
-    //     e.preventDefault();
-    //     $(this).css('background', '#aaaaaa50');
-
-    // });
-
-    // // Drag over
-    // $('div#page').on('dragover', function(e) {
-    //     e.stopPropagation();
-    //     e.preventDefault();
-    // });
-
-
-    // // Drop
-    // $('div#page').on('drop', function(e) {
-
-    //     e.stopPropagation();
-    //     e.preventDefault();
-    //     $(this).css('background', '#fff');
-
-
-    //     var selectedImages = e.originalEvent.dataTransfer.files;
-    //     createFormData(selectedImages);
-    // });
-
-    // function createFormData(images) {
-    //     var fd = new FormData();
-    //     // The FormData interface provides a way to easily construct a set of key/value pairs representing form fields and their values, which can then be easily sent using the XMLHttpRequest.send() method.
-    //     $.each(images, function(i, val) {
-    //         fd.append('userImage[]', val);
-
-    //         //For sending them one by one:
-    //         //fd.append('userImage', val);
-    //         //uploadFormData(fd);
-
-    //     });
-
-    //     uploadFormData(fd);
-    // }
-
 });
+
+// ---------------- Function for adding new image (from Directory) into user's browser ------------------
+
+function updateThePageAfterUploading(src) {
+
+
+    var new_image_container = document.createElement('div');
+    $(new_image_container).addClass("imggalery");
+    new_image_container.innerHTML = `<div class="contentgalery"><img style="margin:5px 5px;" src="${src}" width="160" height="100"></div><div class="btngalery"><img style="cursor:pointer;" src="images/cross.gif"></div>`;
+    var images_zone = $('div#images_zone');
+    images_zone.append(new_image_container);
+
+    images_urls.push(src);
+    images_containers = $('div.imggalery');
+
+
+    $(new_image_container).find('div.btngalery img').on("click", function(event) {
+        crossButtonClick($(event.target));
+    });
+
+}
+
+
+
+
+//----------------- Monitoring the Images directory and adding the new ones -------
+
+function uploadingNewImages() {
+
+    $.ajax({
+        url: "./images.factory.php",
+        type: "GET",
+        data: { function_name: 'imagesFindAllFunc' },
+        dataType: "JSON",
+        success: function(response, status) {
+
+            current_images_urls = response;
+
+            var difference_array = [];
+            difference_array = $(current_images_urls).not(images_urls).get();
+
+            if (current_images_urls.length > images_urls.length) {
+
+                $.each(difference_array, function(index, value) {
+
+                    updateThePageAfterUploading(value);
+                });
+
+            }
+        }
+    });
+
+}
